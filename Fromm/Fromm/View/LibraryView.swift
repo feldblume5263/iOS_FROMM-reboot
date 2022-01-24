263d6377d6c735ed8fee757c7a98c08a669c71de
feldblume5263 <feldblume5263@gmail.com>
2022-01-24 18:54:11 +0900

    feat: #1 library page UI 완성 UI에 맞추어 들어오게 될 DB, 코어데이터 고려해야 함.

--- Fromm/Fromm/View/LibraryView.swift
+++ Fromm/Fromm/View/LibraryView.swift
@@ -10,10 +10,57 @@ import CoreData
 
 struct LibraryView: View {
     var body: some View {
-        List {
-            
+        VStack {
+            HStack {
+                VStack {
+                    Image(uiImage: UIImage(named: "User-Sample") ?? UIImage())
+                        .resizable()
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .clipShape(Circle())
+                        .shadow(radius: 10)
+                        .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                        .scaledToFill()
+                    Text("포뇨")
+                        .font(.headline)
+                }
+                .padding()
+                Spacer()
+            }
+            List {
+                VStack {
+                    Image(uiImage: UIImage(named: "Mission-Library") ?? UIImage())
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: 100, alignment: .center)
+                        .clipped()
+                    HStack {
+                        VStack {
+                            Text("아침 일찍 도서관 가기")
+                                .font(.headline)
+                            Spacer()
+                            Text("주중")
+                                .font(.subheadline)
+                            Text("08:00 AM")
+                                .font(.subheadline)
+                        }
+                    }
+                    .padding()
+                }
+                .listRowInsets(EdgeInsets())
+            }
         }
-        .navigationTitle("Mission Library")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct MissionCell: View {
+    var body: some View {
+        VStack {
+            Image(uiImage: UIImage(named: "Mission-Library") ?? UIImage())
+            VStack {
+                
+            }
+        }
     }
 }
 
