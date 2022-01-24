4db63f1653ef0ad904b922c60722940ffddceba2
feldblume5263 <feldblume5263@gmail.com>
2022-01-24 23:36:59 +0900

    feat: #01 update ui update ui list -> lazyVGrid

--- Fromm/Fromm/View/LibraryView.swift
+++ Fromm/Fromm/View/LibraryView.swift
@@ -9,72 +9,89 @@ import SwiftUI
 import CoreData
 
 struct LibraryView: View {
+    @State var missions = ["", "", "", ""]
+    private let columns: [GridItem] = [GridItem(.flexible(), alignment: .center)]
+    
     var body: some View {
         VStack {
-            HStack {
-                VStack {
-                    Image(uiImage: UIImage(named: "User-Sample") ?? UIImage())
-                        .resizable()
-                        .frame(width: 100, height: 100, alignment: .center)
-                        .clipShape(Circle())
-                        .shadow(radius: 10)
-                        .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-                        .scaledToFill()
-                    Text("포뇨")
-                        .font(.headline)
-                }
-                .padding()
-                Spacer()
-            }
+            UserInfoView()
+            Divider()
             GeometryReader { geo in
-            List {
-                VStack {
-                        Image(uiImage: UIImage(named: "Mission-Library") ?? UIImage())
-                            .resizable()
-                            .scaledToFill()
-                            .frame(height: 100, alignment: .center)
-                            .clipped()
-                        VStack {
-                            VStack {
-                                Text("아침 일찍 도서관 가기")
-                                    .font(.headline)
-                                Spacer()
-                                Text("주중")
-                                    .font(.subheadline)
-                                Text("08:00 AM")
-                                    .font(.subheadline)
-                            }
-                            Spacer()
-                            Button {
-                            } label: {
-                                Text("달성하기")
-                                    .font(.subheadline)
-                                    .foregroundColor(.white)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 20)
-                                            .frame(width: geo.size.width * 0.7)
-                                            .foregroundColor(.mint)
-                                    )
-                            }
-                            
+                ScrollView {
+                    LazyVGrid(columns: columns, alignment: .center, spacing: 10) {
+                        ForEach(0 ..< missions.count, id: \.self) { _ in
+                            MissionCell(geometry: geo)
+                                .listRowInsets(EdgeInsets())
                         }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.secondary, lineWidth: 1)
+                        )
+                        .cornerRadius(10)
                         .padding()
+                    }
                 }
-                .listRowInsets(EdgeInsets())
-            }
             }
         }
+        .navigationTitle("나의 미션")
         .navigationBarTitleDisplayMode(.inline)
     }
 }
 
+struct UserInfoView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Image(uiImage: UIImage(named: "User-Sample") ?? UIImage())
+                    .resizable()
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .clipShape(Circle())
+                    .shadow(radius: 10)
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                    .scaledToFill()
+                Text("포뇨")
+                    .font(.headline)
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
 struct MissionCell: View {
+    let geometry: GeometryProxy
+    
     var body: some View {
         VStack {
             Image(uiImage: UIImage(named: "Mission-Library") ?? UIImage())
+                .resizable()
+                .scaledToFill()
+                .frame(height: 150, alignment: .center)
+                .clipped()
             VStack {
-                
+                VStack {
+                    Text("아침 일찍 도서관 가기")
+                        .font(.headline)
+                    Spacer()
+                    Text("주중")
+                        .font(.subheadline)
+                    Text("08:00 AM")
+                        .font(.subheadline)
+                }
+                Spacer()
+                Button {
+                } label: {
+                    Text("달성하기")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
+                        .background(
+                            RoundedRectangle(cornerRadius: 5)
+                                .frame(width: geometry.size.width * 0.5)
+                                .foregroundColor(.mint))
+                }
             }
+            .padding()
         }
     }
 }
